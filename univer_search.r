3bcc7b6f28c023aa77e9d9e21c043bab34b31571
Sergey <33733128+sergeychuvakin@users.noreply.github.com>
2020-08-17 12:04:02 +0300

    Update univer_search.r

--- univer_search.r
+++ univer_search.r
@@ -6,3 +6,14 @@ if (.Platform$OS.type == 'unix') {
  
 source(readLines('fpath.txt'))
 file.remove('fpath.txt')
+
+
+if (.Platform$OS.type == 'unix') {
+  system('find / -name "CVSM_Modeling_Automation.R" > "fpath.txt"')
+} else if (.Platform$OS.type == 'windows'){
+  shell('dir /S/b "\\CVSM_Modeling_Automation.R" > fpath.txt')
+}
+
+tmp_f <- tools::file_path_as_absolute('fpath.txt')
+setwd(dirname(readLines(tmp_f)))
+file.remove(tmp_f)
